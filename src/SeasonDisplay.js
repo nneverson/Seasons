f32b104e7fdaee49354bcf635b813bf25d39c982
Neely <neely@Neelys-MBP.HPH.Local>
2021-05-08 11:58:13 -0500

    display seasonal icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
+//determine what the season is
+
+const getSeason = (latitude, month) => {
+	if (month > 2 && month < 9) {
+		return latitude > 0 ? 'summer' : 'winter';
+	} else {
+		return latitude > 0 ? 'winter' : 'summer';
+	}
+};
+
 const SeasonDisplay = (props) => {
-	return <div>{props.latitude}</div>;
+	const season = getSeason(props.latitude, new Date().getMonth());
+	const text =
+		season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+	const icon = season === 'winter' ? 'snowflake' : 'sun';
+	console.log(season);
+	return (
+		<div>
+			<i className={`${icon} icon`} />
+			<h1>{text}</h1>
+			<i className={`${icon} icon`} />
+		</div>
+	);
 };
 export default SeasonDisplay;
